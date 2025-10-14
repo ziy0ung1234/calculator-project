86011efad68b34c7d557d3cd10b3431739ddc896
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-10-14 12:48:50 +0900

    [Level2] CalculatorApp에서 Calculator클래스 활용하도록 수정

---
+++ src/com/example/calculator2/CalculatorApp.java
@@ -0,0 +1,10 @@
+package com.example.calculator2;
+import java.util.*;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
+
+    }
+}
